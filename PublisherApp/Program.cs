430efdf22d6a5554fd9ddbfea134759a7efe1eeb
Никита <soloistcsgo@yandex.ru>
2020-08-05 12:32:15 +0300

    publisher and subscriber added

--- PublisherApp/Program.cs
+++ PublisherApp/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace PublisherApp
 {
     class Program
     {
+        static string exchange_1 = "ex_1";
+        static string exchange_2 = "ex_2";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using(var connection = factory.CreateConnection())
+            using(var chanel = connection.CreateModel())
+            {
+                // declaring 2 exchanges  
+                chanel.ExchangeDeclare(exchange_1, ExchangeType.Fanout);
+                chanel.ExchangeDeclare(exchange_2, ExchangeType.Fanout);
+
+                var messageBody = Encoding.ASCII.GetBytes(GetMessage(args));
+
+                var publishProps = chanel.CreateBasicProperties();
+                publishProps.Persistent = true;
+
+                // publish message to exchanges
+                chanel.BasicPublish(exchange_1, "", publishProps, messageBody);
+                chanel.BasicPublish(exchange_2, "", publishProps, messageBody);
+            }
+
         }
+
+        static string GetMessage(string[] args) =>string.Join(" ", args);
+
     }
 }
--- SubscriberApp/Program.cs
+++ SubscriberApp/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace SubscriberApp
 {
     class Program
     {
+        static string exchange_1 = "ex_1";
+        static string exchange_2 = "ex_2";
+        static int messageCounter = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                // declare default queue, which have random name
+                var queueName = chanel.QueueDeclare().QueueName;
+
+                // binding queue to exchanges
+                chanel.QueueBind(queueName, exchange_1, "");
+                chanel.QueueBind(queueName, exchange_2, "");
+
+                // fair
+                chanel.BasicQos(0, 1, false);
+
+                // consuming messages
+                var consumer = new EventingBasicConsumer(chanel);
+                consumer.Received += (sender, ea) =>
+                {
+                    var message = Encoding.ASCII.GetString(ea.Body.ToArray());
+
+                    System.Console.WriteLine($"Queue name is: {queueName}");
+                    ReadMessage(message);
+
+                    chanel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                chanel.BasicConsume(queueName, false, consumer);
+
+                System.Console.WriteLine("Enter to exit...");
+                Console.ReadKey();
+            } 
+        }
+
+        static void ReadMessage(string message)
+        {
+            var currentMessageCount = ++messageCounter;
+
+            System.Console.WriteLine($"Reading new message {currentMessageCount}...");
+            Thread.Sleep(10000);
+            System.Console.WriteLine($"Message {currentMessageCount} is {message}");
         }
     }
 }
